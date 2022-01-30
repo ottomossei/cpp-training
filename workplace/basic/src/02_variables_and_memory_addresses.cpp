993d5a64f76a0dd79620008a0de48feb4080be5a
ottomossei <seki.jobhunting@gmail.com>
2022-01-30 15:53:46 +0900

    Add results

--- workplace/basic/src/02_variables_and_memory_addresses.cpp
+++ workplace/basic/src/02_variables_and_memory_addresses.cpp
@@ -11,3 +11,11 @@ int main() {
     printf("value %f, size %dbyte, address 0x%x\n", c, sizeof(float), &a);
     printf("value %c, size %dbyte, address 0x%x\n", d, sizeof(char), &a);
 }
+
+// results
+/*
+value 100, size 4byte, address 0x7809ff9c
+value 1.200000, size 8byte, address 0x7809ff9c
+value 123.400002, size 4byte, address 0x7809ff9c
+value a, size 1byte, address 0x7809ff9c
+*/
