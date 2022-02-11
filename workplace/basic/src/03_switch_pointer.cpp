0359674d973041023d23cf360a1ee99c8e4aaa17
ottomossei <seki.jobhunting@gmail.com>
2022-02-11 15:58:44 +0900

    Adjust indentation

--- workplace/basic/src/03_switch_pointer.cpp
+++ workplace/basic/src/03_switch_pointer.cpp
@@ -1,23 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 void show(int, int, int);
 
-int main() {
+int main()
+{
     int a = 100;
     int b = 200;
     int *p = NULL;
-    p=&a;
+    p = &a;
     show(a, b, *p);
-    *p=300;
+    *p = 300;
     show(a, b, *p);
-    p=&b;
+    p = &b;
     show(a, b, *p);
-    *p=400;
+    *p = 400;
     show(a, b, *p);
-
 }
 
-void show(int n1, int n2, int n3){
+void show(int n1, int n2, int n3)
+{
     printf("a=%d b=%d *p=%d\n", n1, n2, n3);
 }
 
@@ -27,4 +28,18 @@ a=100 b=200 *p=100
 a=300 b=200 *p=300
 a=300 b=200 *p=200
 a=300 b=400 *p=400
+*/
+
+/* Points
+1.  var  : value
+    &var : address
+    p    : address (variable)
+    *p   : value (variable)
+    &p   : address (p-self)
+2.  A pointer is a place to store an address.
+3. *p means pointer.(int *p = pointer)
+    A pointer that is assigned NULL is called a null pointer.
+    if(ptr)     // succeeds if p is not null
+    if(!ptr)    // succeeds if p is null
+
 */
