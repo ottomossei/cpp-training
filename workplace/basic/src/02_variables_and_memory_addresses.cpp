60f37c2e74f7e40192a24fbaf1928492d22af8d9
ottomossei <seki.jobhunting@gmail.com>
2022-02-11 15:58:23 +0900

    Add Point

--- workplace/basic/src/02_variables_and_memory_addresses.cpp
+++ workplace/basic/src/02_variables_and_memory_addresses.cpp
@@ -1,11 +1,12 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     int a = 100;
     double b = 1.2;
     float c = 123.4f;
-    char d= 'a';
+    char d = 'a';
     printf("value %d, size %dbyte, address 0x%x\n", a, sizeof(int), &a);
     printf("value %f, size %dbyte, address 0x%x\n", b, sizeof(double), &a);
     printf("value %f, size %dbyte, address 0x%x\n", c, sizeof(float), &a);
@@ -18,4 +19,10 @@ value 100, size 4byte, address 0x7809ff9c
 value 1.200000, size 8byte, address 0x7809ff9c
 value 123.400002, size 4byte, address 0x7809ff9c
 value a, size 1byte, address 0x7809ff9c
+*/
+
+/* Points
+1. Each type has different bytes.(sizeof)
+2. & means address
+3. 0x means hexadecimal
 */
