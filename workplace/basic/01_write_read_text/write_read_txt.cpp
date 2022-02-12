757e9334eaafb5dbff079510177451b4e3f6dc0d
ottomossei <seki.jobhunting@gmail.com>
2022-02-12 10:34:26 +0900

    Add

---
+++ workplace/basic/01_write_read_text/write_read_txt.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    FILE *file;
+    file = fopen("./sample_write.txt", "w");
+    if (file == NULL)
+    {
+        printf("Don't open this file.");
+        exit(1);
+    }
+    else
+    {
+        fprintf(file, "Hello World.\r\n");
+        fprintf(file, "ABCDEF\r\n");
+        fclose(file);
+    }
+}
+
+/* Points
+1.  exit(1) : Abnormal termination of the program
+2.  if (file ==NULL){
+    This program is executed, for example,
+    when the target directory does not exist or when you do not have permission to browse it.
+*/
